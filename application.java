51071e151dd70c117a3be20cd9dfca11df0bc51e
thummalapallisudhakar <thummalapallisudhakar501@gmail.com>
2025-08-06 08:40:55 +0530

    appication.java file is modified

--- application.java
+++ application.java
@@ -3,4 +3,18 @@ public class HelloWorld {
         // This line prints "Hello, World!" to the console
         System.out.println("Hello, World!");
     }
+#making changes of appication.java file
+class Main {
+
+  public static void main(String[] args) {
+    
+    int first = 10;
+    int second = 20;
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
+  }
+}
+#end of changes
 }
